2e8993541274ed07353d14b9628499ab33958d80
KatsuyukiToki <katuyuki.toki@gmail.com>
2020-02-13 01:10:08 +0900

    キャンドル1分足

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -6,12 +6,18 @@ import (
 	"net/http"
 
 	"gotrading/config"
+	"gotrading/app/models"
 )
 
 var templates = template.Must(template.ParseFiles("app/views/chart.html"))
 
 func viewChartHandler(w http.ResponseWriter, r *http.Request) {
-	err := templates.ExecuteTemplate(w, "chart.html", nil)
+	limit := 100
+	duration := "1m"
+	durationTime := config.Config.Durations[duration]
+	df, _ := models.GetAllCandle(config.Config.ProductCode, durationTime, limit)
+
+	err := templates.ExecuteTemplate(w, "chart.html", df.Candles)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module gotrading
+
+go 1.13
+
+require (
+	github.com/gorilla/websocket v1.4.1
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible
+	gopkg.in/ini.v1 v1.52.0
+)
