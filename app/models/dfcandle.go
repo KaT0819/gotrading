4c64ec430186397b7ca34b66ada4b584b3be66ac
KatsuyukiToki <katuyuki.toki@gmail.com>
2020-02-16 15:55:55 +0900

    Macd

--- app/models/dfcandle.go
+++ app/models/dfcandle.go
@@ -16,6 +16,7 @@ type DataFrameCandle struct {
 	BBands        *BBands        `json:"bbands,omitempty"`
 	IchimokuCloud *IchimokuCloud `json:"ichimoku,omitempty"`
 	Rsi           *Rsi           `json:"rsi,omitempty"`
+	Macd          *Macd          `json:"macd,omitempty"`
 }
 
 // Sma 移動平均線
@@ -52,6 +53,16 @@ type IchimokuCloud struct {
 type Rsi struct {
 	Period int       `json:"period,omitempty"`
 	Values []float64 `json:"values,omitempty"`
+}
+
+// Macd
+type Macd struct {
+	FastPeriod   int       `json:"fast_period,omitempty"`
+	SlowPeriod   int       `json:"slow_period,omitempty"`
+	SignalPeriod int       `json:"signal_period,omitempty"`
+	Macd         []float64 `json:"macd,omitempty"`
+	MacdSignal   []float64 `json:"macd_signal,omitempty"`
+	MacdHist     []float64 `json:"macd_hist,omitempty"`
 }
 
 func (df *DataFrameCandle) Times() []time.Time {
@@ -161,6 +172,22 @@ func (df *DataFrameCandle) AddRsi(period int) bool {
 		df.Rsi = &Rsi{
 			Period: period,
 			Values: values,
+		}
+		return true
+	}
+	return false
+}
+
+func (df *DataFrameCandle) AddMacd(inFastPeriod, inSlowPeriod, inSignalPeriod int) bool {
+	if len(df.Candles) > 1 {
+		outMACD, outMACDSignal, outMACDHist := talib.Macd(df.Closes(), inFastPeriod, inSlowPeriod, inSignalPeriod)
+		df.Macd = &Macd{
+			FastPeriod:   inFastPeriod,
+			SlowPeriod:   inSlowPeriod,
+			SignalPeriod: inSignalPeriod,
+			Macd:         outMACD,
+			MacdSignal:   outMACDSignal,
+			MacdHist:     outMACDHist,
 		}
 		return true
 	}
